1ef76f1964d26a2732268b21128c716b65fdf1b7
LastAeon <syihabuddiny@gmail.com>
2022-07-01 09:59:24 +0700

    re-initialize java spring

--- simpleCRUD/src/test/java/com/simplecrud/simplecrud/SimpleCrudApplicationTests.java
+++ simpleCRUD/src/test/java/com/simplecrud/simplecrud/SimpleCrudApplicationTests.java
@@ -1,13 +1,13 @@
-package com.simplecrud.javaspringcrud;
+package com.simplecrud.simplecrud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaSpringCrudApplicationTests {
+class SimpleCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
